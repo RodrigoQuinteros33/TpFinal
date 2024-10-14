f99fa8696c6e21315d9056fdf9e1822c303a5f33
RodrigoQuinteros33 <rodrigoj.quinteros@gmail.com>
2024-10-14 18:23:13 -0300

    first commit

---
+++ src/Policia.java
@@ -0,0 +1,20 @@
+public class Policia {
+    private String nombre;
+    private String apellido;
+    private int legajo;
+
+    public Policia (String nombre, String apellido, int legajo){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.legajo = legajo;
+    }
+
+    @Override
+    public String toString() {
+        return "Policia{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", legajo=" + legajo +
+                '}';
+    }
+}
