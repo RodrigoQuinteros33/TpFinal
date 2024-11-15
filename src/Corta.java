355a78854ba085bfcb262f252a7637aa220178ab
RodrigoQuinteros33 <rodrigoj.quinteros@gmail.com>
2024-11-15 17:08:30 -0300

    cambionuevo

--- src/Corta.java
+++ src/Corta.java
@@ -2,6 +2,11 @@ public class Corta extends Arma{
 
     private boolean esAutomatica;
 
+    public Corta(int cantMuniciones, double alcance, String marca, int calibre, String estado, boolean esAutomatica) {
+        super(cantMuniciones, alcance, marca, calibre, estado);
+        this.esAutomatica = esAutomatica;
+    }
+
     public boolean efectividadMts(){
         return false;
     }
